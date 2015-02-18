48e5082855a5e9a95d00f06e3d9825ec56a06236
qizhenyu <chikchunyu@gmail.com>
2015-02-18 11:56:34 +0800

    修改订阅源

--- js/base.js
+++ js/base.js
@@ -12,7 +12,7 @@ var next = new Vue({
 })
 
 //Hacker News
-qwest.get('http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=8&q=http%3A%2F%2Fnews.google.com%2Fnews%3Foutput%3Drss').then(function(res){
+qwest.get('https://www.v2ex.com/api/topics/hot.json').then(function(res){
   hackernews.lists = JSON.parse(res);
 })
 var hackernews = new Vue({
